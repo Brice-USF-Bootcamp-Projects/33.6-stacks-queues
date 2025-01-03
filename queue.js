1699884eae5b3ac2acadac5a0303366072ee3733
Brice Nelson <bnelsonemail@icloud.com>
2025-01-03 17:12:14 -0500

    add logic for queues and stacks

--- queue.js
+++ queue.js
@@ -2,45 +2,62 @@
 
 class Node {
   constructor(val) {
-    this.val = val;
-    this.next = null;
+    this.val = val; // Stores the value of the node
+    this.next = null; // Points to the next node in the queue
   }
 }
 
 /** Queue: chained-together nodes where you can
  *  remove from the front or add to the back. */
-
 class Queue {
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+    this.first = null; // Points to the first node in the queue
+    this.last = null;  // Points to the last node in the queue
+    this.size = 0;     // Tracks the number of nodes in the queue
   }
 
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
-
   enqueue(val) {
-
+    const newNode = new Node(val);
+    if (!this.last) { // If the queue is empty
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode; // Link the new node at the end
+      this.last = newNode;      // Update the last pointer
+    }
+    this.size++;
+    return undefined;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
-
   dequeue() {
-
+    if (!this.first) { // If the queue is empty
+      throw new Error("Queue is empty");
+    }
+    const dequeuedValue = this.first.val; // Get the value to return
+    this.first = this.first.next;         // Move the first pointer
+    this.size--;
+    if (this.size === 0) { // If the queue becomes empty
+      this.last = null;    // Update the last pointer
+    }
+    return dequeuedValue;
   }
 
   /** peek(): return the value of the first node in the queue. */
-
   peek() {
-
+    if (!this.first) { // If the queue is empty
+      throw new Error("Queue is empty");
+    }
+    return this.first.val; // Return the value at the front
   }
 
-  /** isEmpty(): return true if the queue is empty, otherwise false */
-
+  /** isEmpty(): return true if the queue is empty, otherwise false. */
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
 module.exports = Queue;
+
--- stack.js
+++ stack.js
@@ -9,37 +9,53 @@ class Node {
 
 /** Stack: chained-together nodes where you can
  *  remove from the top or add to the top. */
-
 class Stack {
   constructor() {
-    this.first = null;
-    this.last = null;
-    this.size = 0;
+    this.first = null; // Top of the stack
+    this.last = null;  // Bottom of the stack
+    this.size = 0;     // Tracks the number of nodes in the stack
   }
 
-  /** push(val): add new value to end of the stack. Returns undefined. */
-
+  /** push(val): add new value to the top of the stack. Returns undefined. */
   push(val) {
-
+    const newNode = new Node(val);
+    if (!this.first) { // If the stack is empty
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      newNode.next = this.first; // Point the new node to the current top
+      this.first = newNode;      // Update the top of the stack
+    }
+    this.size++;
+    return undefined;
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
-
   pop() {
-
+    if (!this.first) { // If the stack is empty
+      throw new Error("Stack is empty");
+    }
+    const poppedValue = this.first.val; // Get the value to return
+    this.first = this.first.next;       // Move the top pointer
+    this.size--;
+    if (this.size === 0) { // If the stack becomes empty
+      this.last = null;    // Update the bottom pointer
+    }
+    return poppedValue;
   }
 
   /** peek(): return the value of the first node in the stack. */
-
   peek() {
-
+    if (!this.first) { // If the stack is empty
+      throw new Error("Stack is empty");
+    }
+    return this.first.val;
   }
 
-  /** isEmpty(): return true if the stack is empty, otherwise false */
-
+  /** isEmpty(): return true if the stack is empty, otherwise false. */
   isEmpty() {
-
+    return this.size === 0;
   }
 }
 
